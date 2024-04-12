365fa85fe1bc281ebf947608ddae92e53e512523
EC2 Default User <ec2-user@ip-192-168-0-66.ap-northeast-1.compute.internal>
2024-04-12 11:08:33 +0000

    [Fix]admin.rb calendar.rb

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -5,4 +5,5 @@ class Calendar < ApplicationRecord
   validates :title, presence: true, length: { maximum: 20 }
   validates :content, length: { maximum: 100 }
   validates :start_time, presence: true
+
 end
